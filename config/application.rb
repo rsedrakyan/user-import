207b9eac3fdd9d7c9b9e46101f327470ed7cfd85
rsedrakyan <rafayel@eventgeek.com>
2024-06-20 01:38:43 +0100

    Add active_job railtie as turbo-rails depends on it
    Some of the turbo functionality (e.g. broadcast later) which
    is not used in this project requires ActiveJob. As it cannot
    be disabled at the moment, enabling the railtie is the only
    solution.
    
    See: https://github.com/hotwired/turbo-rails/pull/602

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require_relative 'boot'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
+require 'active_job/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
